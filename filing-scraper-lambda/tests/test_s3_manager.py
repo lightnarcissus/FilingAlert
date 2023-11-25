196d07832af2a81de53410b6d81b03056346300e
apatel-tl <lightnarcissus@users.noreply.github.com>
2023-11-25 12:01:44 -0500

    Add mock tests

--- filing-scraper-lambda/tests/test_s3_manager.py
+++ filing-scraper-lambda/tests/test_s3_manager.py
@@ -1,7 +1,21 @@
-import pytest
+import unittest
+from unittest import mock
 
-def test_s3_manager_instance():
-    from filing_scraper.s3_manager import S3Manager
-    inst = S3Manager()
-    assert inst is not None
-    assert isinstance(inst, S3Manager)
+class S3ManagerTest(unittest.TestCase):
+    def test_s3_manager_instance(self):
+        from filing_scraper.s3_manager import S3Manager
+        inst = S3Manager()
+        assert inst is not None
+        assert isinstance(inst, S3Manager)
+
+    @mock.patch('filing_scraper.s3_manager.S3Manager.update_file')
+    def test_s3_manager_update_file(self, mock_s3_client):
+         self.assertTrue(mock_s3_client.return_value is not None)
+    @mock.patch('filing_scraper.s3_manager.S3Manager.get_current_file')
+    def test_s3_manager_get_current_file(self, mock_s3_client):
+        from filing_scraper.s3_manager import S3Manager
+        mock_s3_client.return_value = "mock_key"
+        inst = S3Manager()
+        result = inst.get_current_file("mock_key")
+        assert result is "mock_key"
+        assert isinstance(result, str)
--- filing-scraper-lambda/tests/test_sns_publisher.py
+++ filing-scraper-lambda/tests/test_sns_publisher.py
@@ -1,8 +1,17 @@
-import pytest
+import unittest
+from unittest import mock
 
-
-def test_sns_publisher_instance():
-    from filing_scraper.sns_publisher import SNSPublisher
-    inst = SNSPublisher()
-    assert inst is not None
-    assert isinstance(inst, SNSPublisher)
+class SNSPublisherTest(unittest.TestCase):
+    def test_sns_publisher_instance(self):
+        from filing_scraper.sns_publisher import SNSPublisher
+        inst = SNSPublisher()
+        assert inst is not None
+        assert isinstance(inst, SNSPublisher)
+    
+    @mock.patch('filing_scraper.sns_publisher.SNSPublisher.publish_email')
+    def test_sns_publisher_publish(self, mock_sns_client):
+        from filing_scraper.sns_publisher import SNSPublisher
+        inst = SNSPublisher()
+        mock_sns_client.return_value = None
+        self.assertTrue(inst.publish_email("test_subj", "test_msg") is None)
+        
